cfaf0c63572e483a9d9b5485089f81b0555a392c
MSI\Nico <lepilolodior7633@gmail.com>
2019-01-21 15:27:13 +0100

    Setup Menus

---
+++ Assets/Scripts/GameManager/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject optionsMenu;
+    public GameObject musicSelection;
+    public GameObject mainMenu;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnClickedButtonPlay()
+    {
+        SceneManager.LoadScene("TheoTestScene");
+    }
+
+    public void OnClickedButtonPlayMusicSelection()
+    {
+        musicSelection.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void OnClickedButtonPlayBack()
+    {
+        musicSelection.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void OnClickedButtonQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickedButtonShop()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    public void OnClickedButtonOption()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void OnClickedButtonOptionBack()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
