f51afb6240ac6b448c3308ee2647822f6fab905f
MSI\Nico <lepilolodior7633@gmail.com>
2019-01-28 17:05:07 +0100

    MAJ 17h

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -2,22 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
-
-    public GameObject EnemyPawn;
-
-	// Use this for initialization
-	void Start () {
-        SpawnAnEnemy();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-           
-        }
-
-    public void SpawnAnEnemy()
+public class EnemySpawner : MonoBehaviour
+{
+    // Normal
+    public float maxTimeEnemyNormal = 5;
+    public float minTimeEnemyNormal = 2;
+    private float spawnTimeEnemyNormal;
+    private float timeEnemyNormal;
+    public GameObject EnemyPawnNormal;
+    // Swipe
+    public float maxTimeEnemySwipe = 7;
+    public float minTimeEnemySwipe = 3;
+    private float spawnTimeEnemySwipe;
+    private float timeEnemySwipe;
+    public GameObject EnemyPawnSwipe;
+    // Drag
+    public float maxTimeEnemyDrag = 9;
+    public float minTimeEnemyDrag = 5;
+    private float spawnTimeEnemyDrag;
+    private float timeEnemyDrag;
+    public GameObject EnemyPawnDrag;
+    
+    // Use this for initialization
+    void Start ()
     {
-        Instantiate(EnemyPawn, transform.position, Quaternion.identity);
+        SetRandomTimeNormal();
+        SetRandomTimeSwipe();
+        SetRandomTimeDrag();
+        timeEnemyNormal = minTimeEnemyNormal;
+        timeEnemySwipe = minTimeEnemySwipe;
+        timeEnemyDrag = minTimeEnemyDrag;
+    }
+
+    void FixedUpdate()
+    {
+        timeEnemyNormal += Time.deltaTime;
+        timeEnemySwipe += Time.deltaTime;
+        timeEnemyDrag += Time.deltaTime;
+        // Normal
+        if (timeEnemyNormal >= spawnTimeEnemyNormal)
+        {
+            SpawnObjectNormal();
+            SetRandomTimeNormal();
+            timeEnemySwipe -= 1f;
+            timeEnemyDrag -= 1f;
+        }
+        // Swipe
+        else if (timeEnemySwipe >= spawnTimeEnemySwipe)
+        {
+            SpawnObjectSwipe();
+            SetRandomTimeSwipe();
+            timeEnemyNormal -= 1f;
+            timeEnemyDrag -= 1f;
+        }
+        // Drag
+        else if (timeEnemyDrag >= spawnTimeEnemyDrag)
+        {
+            SpawnObjectDrag();
+            SetRandomTimeDrag();
+            timeEnemyNormal -= 1f;
+            timeEnemySwipe -= 1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+           
+    }
+
+    void SetRandomTimeNormal()
+    {
+        spawnTimeEnemyNormal = Random.Range(minTimeEnemyNormal, maxTimeEnemyNormal);
+    }
+
+    void SetRandomTimeSwipe()
+    {
+        spawnTimeEnemySwipe = Random.Range(minTimeEnemySwipe, maxTimeEnemySwipe);
+    }
+
+    void SetRandomTimeDrag()
+    {
+        spawnTimeEnemyDrag = Random.Range(minTimeEnemyDrag, maxTimeEnemyDrag);
+    }
+
+    void SpawnObjectNormal()
+    {
+        timeEnemyNormal = 0f;
+        Instantiate(EnemyPawnNormal, transform.position, Quaternion.identity);
+    }
+
+    void SpawnObjectSwipe()
+    {
+        timeEnemySwipe = 0f;
+        Instantiate(EnemyPawnSwipe, transform.position, Quaternion.identity);
+    }
+
+    void SpawnObjectDrag()
+    {
+        timeEnemyDrag = 0f;
+        Instantiate(EnemyPawnDrag, transform.position, Quaternion.identity);
     }
 }
