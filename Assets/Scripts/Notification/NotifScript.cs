1d2d4f3224efaeb21e6eb77dee453cd8f30b811e
Vil_Coyote <ghost64400@gmail.com>
2019-03-04 15:28:07 +0100

    Test notification Scene test ludo

---
+++ Assets/Scripts/Notification/NotifScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Assets.SimpleAndroidNotifications
+{
+
+    public class NotifScript : MonoBehaviour
+    {
+
+
+        public void SendNotif()
+        {
+            NotificationManager.Send(TimeSpan.FromSeconds(10), "PhonM", "An intruder is partying hard in your phone, you should check what is going on", Color.cyan);
+        }
+
+
+        void Update()
+        {
+
+
+
+        }
+
+    }
+}
